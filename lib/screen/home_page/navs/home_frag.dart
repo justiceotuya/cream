b537b021221c6f5e7105d637cb2e727b26a3801a
mac <vsquare396@gmail.com>
2020-12-25 13:47:27 +0100

    display content history to home screen

--- lib/screen/home_page/navs/home_frag.dart
+++ lib/screen/home_page/navs/home_frag.dart
@@ -1,9 +1,11 @@
+import 'package:cream_platform_app/apis/content/personal/provider/history_providers.dart';
 import 'package:cream_platform_app/data/temp/ads_banner_mock_data.dart';
 import 'package:cream_platform_app/helper/image_loader_helper.dart';
 import 'package:cream_platform_app/resources/image_resources.dart';
 import 'package:cream_platform_app/screen/ui/category_widget.dart';
 import 'package:cream_platform_app/screen/ui/custom_search_box.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -12,6 +14,17 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  ContentHistoryProviders _contentHistoryProviders;
+
+  @override
+  void initState() {
+    _contentHistoryProviders =
+        Provider.of<ContentHistoryProviders>(context, listen: false);
+    _contentHistoryProviders.initialize(context);
+    _contentHistoryProviders.initScrollListener();
+    _contentHistoryProviders.history();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,6 +60,7 @@ class _HomeState extends State<Home> {
         CategoryWidget(
           icon: bid,
           catName: 'Cream Bid',
+          type: 'IMAGE',
         ),
         SizedBox(
           height: 20,
@@ -54,6 +68,7 @@ class _HomeState extends State<Home> {
         CategoryWidget(
           icon: music,
           catName: 'Cream Music',
+          type: 'MUSIC',
         ),
         SizedBox(
           height: 20,
@@ -61,6 +76,7 @@ class _HomeState extends State<Home> {
         CategoryWidget(
           icon: video,
           catName: 'Cream Video',
+          type: 'VIDEO',
         ),
         SizedBox(
           height: 20,
@@ -68,6 +84,7 @@ class _HomeState extends State<Home> {
         CategoryWidget(
           icon: image,
           catName: 'Cream Images',
+          type: 'IMAGE',
         ),
         SizedBox(
           height: 30,
--- lib/screen/ui/category_widget.dart
+++ lib/screen/ui/category_widget.dart
@@ -1,277 +1,155 @@
+import 'package:cream_platform_app/apis/content/personal/provider/history_providers.dart';
 import 'package:cream_platform_app/helper/helper.dart';
 import 'package:cream_platform_app/helper/image_loader_helper.dart';
 import 'package:cream_platform_app/resources/color_resources.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'text_view_widget.dart';
 
 class CategoryWidget extends StatelessWidget {
   final String icon;
   final String catName;
+  final String type;
   final VoidCallback onItemClicked;
   final VoidCallback onSeeAllTapped;
 
   CategoryWidget(
       {@required this.icon,
       @required this.catName,
+      @required this.type,
       this.onItemClicked,
       this.onSeeAllTapped});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              mainAxisSize: MainAxisSize.min,
+    return Consumer<ContentHistoryProviders>(
+        builder: (_, provider, __) => Column(
               children: [
-                ImageLoader(
-                  path: icon,
-                ),
-                TextViewWidget(
-                  text: catName,
-                  textSize: 14,
-                  textAlign: TextAlign.left,
-                  maxLines: 1,
-                  color: red,
-                  fontWeight: FontWeight.normal,
-                  fontStyle: FontStyle.normal,
-                ),
-                SizedBox(
-                  width: 5,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        ImageLoader(
+                          path: icon,
+                        ),
+                        TextViewWidget(
+                          text: catName,
+                          textSize: 14,
+                          textAlign: TextAlign.left,
+                          maxLines: 1,
+                          color: red,
+                          fontWeight: FontWeight.normal,
+                          fontStyle: FontStyle.normal,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Container(
+                          height: 14,
+                          width: 1,
+                          color: textColor4,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        TextViewWidget(
+                          text: 'Recently Added',
+                          textSize: 14,
+                          textAlign: TextAlign.left,
+                          maxLines: 1,
+                          color: textColor4,
+                          fontWeight: FontWeight.normal,
+                          fontStyle: FontStyle.normal,
+                        ),
+                      ],
+                    ),
+                    TextViewWidget(
+                      onTap: () {},
+                      text: 'See all',
+                      textSize: 14,
+                      textAlign: TextAlign.left,
+                      maxLines: 1,
+                      color: red,
+                      fontWeight: FontWeight.normal,
+                      fontStyle: FontStyle.normal,
+                    ),
+                  ],
                 ),
                 Container(
-                  height: 14,
-                  width: 1,
-                  color: textColor4,
-                ),
-                SizedBox(
-                  width: 5,
-                ),
-                TextViewWidget(
-                  text: 'Recently Added',
-                  textSize: 14,
-                  textAlign: TextAlign.left,
-                  maxLines: 1,
-                  color: textColor4,
-                  fontWeight: FontWeight.normal,
-                  fontStyle: FontStyle.normal,
-                ),
+                  width: getWidth(context),
+                  height: 130,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: white,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey[300],
+                          offset: Offset(0.0, 0.0), //(x,y)
+                          blurRadius: 2.0,
+                          spreadRadius: 0.1),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(11.0),
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: provider.data
+                          .map((data) => data?.type != type ? Container() : Container(
+                                margin: EdgeInsets.only(right: 12),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                      height: 67,
+                                      width: 67,
+                                      decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.circular(8.0),
+                                        color: white,
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color: yellowSplash,
+                                            offset: Offset(0.0, 1.0), //(x,y)
+                                            blurRadius: 1.0,
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 6,
+                                    ),
+                                    TextViewWidget(
+                                      text: data?.content ?? 'Content',
+                                      textSize: 12,
+                                      textAlign: TextAlign.left,
+                                      maxLines: 1,
+                                      color: textColor1,
+                                      fontWeight: FontWeight.w400,
+                                      fontStyle: FontStyle.normal,
+                                    ),
+                                    SizedBox(
+                                      height: 2,
+                                    ),
+                                    TextViewWidget(
+                                      text: data?.name ?? '',
+                                      textSize: 10,
+                                      textAlign: TextAlign.left,
+                                      maxLines: 1,
+                                      color: textColor1,
+                                      fontWeight: FontWeight.normal,
+                                      fontStyle: FontStyle.normal,
+                                    ),
+                                  ],
+                                ),
+                              ))
+                          .toList(),
+                    ),
+                  ),
+                )
               ],
-            ),
-            TextViewWidget(
-              onTap: () {},
-              text: 'See all',
-              textSize: 14,
-              textAlign: TextAlign.left,
-              maxLines: 1,
-              color: red,
-              fontWeight: FontWeight.normal,
-              fontStyle: FontStyle.normal,
-            ),
-          ],
-        ),
-        Container(
-          width: getWidth(context),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10.0),
-            color: white,
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.grey[300],
-                  offset: Offset(0.0, 0.0), //(x,y)
-                  blurRadius: 2.0,
-                  spreadRadius: 0.1),
-            ],
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(11.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      height: 67,
-                      width: 67,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8.0),
-                        color: white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: yellowSplash,
-                            offset: Offset(0.0, 1.0), //(x,y)
-                            blurRadius: 1.0,
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 6,
-                    ),
-                    TextViewWidget(
-                      text: 'Title',
-                      textSize: 12,
-                      textAlign: TextAlign.left,
-                      maxLines: 1,
-                      color: textColor1,
-                      fontWeight: FontWeight.w400,
-                      fontStyle: FontStyle.normal,
-                    ),
-                    SizedBox(
-                      height: 2,
-                    ),
-                    TextViewWidget(
-                      text: 'Artiste',
-                      textSize: 10,
-                      textAlign: TextAlign.left,
-                      maxLines: 1,
-                      color: textColor1,
-                      fontWeight: FontWeight.normal,
-                      fontStyle: FontStyle.normal,
-                    ),
-                  ],
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      height: 67,
-                      width: 67,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8.0),
-                        color: white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: yellowSplash,
-                            offset: Offset(0.0, 1.0), //(x,y)
-                            blurRadius: 1.0,
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 6,
-                    ),
-                    TextViewWidget(
-                      text: 'Title',
-                      textSize: 12,
-                      textAlign: TextAlign.left,
-                      maxLines: 1,
-                      color: textColor1,
-                      fontWeight: FontWeight.w400,
-                      fontStyle: FontStyle.normal,
-                    ),
-                    SizedBox(
-                      height: 2,
-                    ),
-                    TextViewWidget(
-                      text: 'Artiste',
-                      textSize: 10,
-                      textAlign: TextAlign.left,
-                      maxLines: 1,
-                      color: textColor1,
-                      fontWeight: FontWeight.normal,
-                      fontStyle: FontStyle.normal,
-                    ),
-                  ],
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      height: 67,
-                      width: 67,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8.0),
-                        color: white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: yellowSplash,
-                            offset: Offset(0.0, 1.0), //(x,y)
-                            blurRadius: 1.0,
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 6,
-                    ),
-                    TextViewWidget(
-                      text: 'Title',
-                      textSize: 12,
-                      textAlign: TextAlign.left,
-                      maxLines: 1,
-                      color: textColor1,
-                      fontWeight: FontWeight.w400,
-                      fontStyle: FontStyle.normal,
-                    ),
-                    SizedBox(
-                      height: 2,
-                    ),
-                    TextViewWidget(
-                      text: 'Artiste',
-                      textSize: 10,
-                      textAlign: TextAlign.left,
-                      maxLines: 1,
-                      color: textColor1,
-                      fontWeight: FontWeight.normal,
-                      fontStyle: FontStyle.normal,
-                    ),
-                  ],
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      height: 67,
-                      width: 67,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8.0),
-                        color: white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: yellowSplash,
-                            offset: Offset(0.0, 1.0), //(x,y)
-                            blurRadius: 1.0,
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 6,
-                    ),
-                    TextViewWidget(
-                      text: 'Title',
-                      textSize: 12,
-                      textAlign: TextAlign.left,
-                      maxLines: 1,
-                      color: textColor1,
-                      fontWeight: FontWeight.w400,
-                      fontStyle: FontStyle.normal,
-                    ),
-                    SizedBox(
-                      height: 2,
-                    ),
-                    TextViewWidget(
-                      text: 'Artiste',
-                      textSize: 10,
-                      textAlign: TextAlign.left,
-                      maxLines: 1,
-                      color: textColor1,
-                      fontWeight: FontWeight.normal,
-                      fontStyle: FontStyle.normal,
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        )
-      ],
-    );
+            ));
   }
 }
